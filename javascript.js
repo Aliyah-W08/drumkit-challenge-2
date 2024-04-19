b0bcfa6c6af2821859464e678882492c94391c1a
Aliyah-W08 <aliyahwaris278@gmail.com>
2024-04-19 14:02:36 +0100

    first commit

--- javascript.js
+++ javascript.js
@@ -1,13 +1,13 @@
 //button elements
-const boomButton = document.getElementsByClassName("a button")
-const clapButton = document.getElementsByClassName("s button");
-const hihatButton = document.getElementsByClassName("d button");
-const kickButton = document.getElementsByClassName("f button");
-const openhatButton = document.getElementsByClassName("h button");
-const rideButton = document.getElementsByClassName("j button");
-const snareButton = document.getElementsByClassName("k button");
-const tinkButton = document.getElementsByClassName("l button");
-const tomButton = document.getElementsByClassName("p button");
+const boomButton = document.getElementById("a button")
+const clapButton = document.getElementById("s button");
+const hihatButton = document.getElementById("d button");
+const kickButton = document.getElementById("f button");
+const openhatButton = document.getElementById("h button");
+const rideButton = document.getElementById("j button");
+const snareButton = document.getElementById("k button");
+const tinkButton = document.getElementById("l button");
+const tomButton = document.getElementById("p button");
 
 //sound elements
 const boomSound = document.getElementById("boomSound");
@@ -19,6 +19,7 @@ const rideSound = document.getElementById("rideSound");
 const snareSound = document.getElementById("snareSound");
 const tinkSound = document.getElementById("tinkSound");
 const tomSound = document.getElementById("tomSound");
+
 
 //each button when pressed will play the sound assigned.
 boomButton.addEventListener("click", () => {
@@ -56,3 +57,41 @@ tinkButton.addEventListener("click", () => {
 tomButton.addEventListener("click", () => {
     tomSound.play()
 });
+
+//pressKey
+document.addEventListener('keydown', function (event) {
+    // Check which key is pressed
+    switch (event.key) {
+        case "a":
+            // Play boomSound when "a" is pressed
+            document.getElementById("boomSound").play();
+            break;
+
+        case "s": // this is changing the key you want pressed
+            // Play clapSound when "b" is pressed
+            document.getElementById("clapSound").play();
+            break;//the break will stop the previous code running and move to the next
+        case "d":
+            document.getElementById("hihatSound").play();
+            break;
+        case "f":
+            document.getElementById("kickSound").play();
+            break;
+        case "h":
+            document.getElementById("openhatSound").play();
+            break;
+        case "j":
+            document.getElementById("rideSound").play();
+            break;
+        case "k":
+            document.getElementById("snareSound").play();
+            break;
+        case "l":
+            document.getElementById("tinkSound").play();
+            break;
+        case "p":
+            document.getElementById("tomSound").play();
+    }
+});
+
+
